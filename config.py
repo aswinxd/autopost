72c0b15c1473f65227f990acc389e1e31516619f
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-03-23 11:22:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+
+TOKEN = 'your_bot_token'
+
+# Source and target channel IDs
+SOURCE_CHANNEL_ID = -1001234567890  # Replace with your source channel ID
+TARGET_CHANNEL_ID = -1009876543210  # Replace with your target channel ID
