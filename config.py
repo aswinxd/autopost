aa8c405c05d3387c0f35115c258b20bbb95c5bec
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-03-23 13:22:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-TOKEN = '6765987978:AAHuqASdsQPFUV2O28nbx9nbM0QbTwHKL4U' #Source and target channel IDs
+TOKEN = '6765987978:AAHuqASdsQPFUV2O28nbx9nbM0QbTwHKL4U'
+API_ID =
+API_HASH =''      #Source and target channel IDs
 SOURCE_CHANNEL_ID = -1002023444138  # Replace with your source channel ID 
 TARGET_CHANNEL_ID = -1002059668597  # Replace with your target channel ID
